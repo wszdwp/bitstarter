a4ee55783ae7f65779c09e929e6bc1249f918a00
wszdwp <pan.wang@stonybrook.edu>
2013-09-01 20:52:17 +0000

    updated web.js

--- web.js
+++ web.js
@@ -4,14 +4,14 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-var buf = new Buffer(256);
+//var buf = new Buffer(256);
 
 var htmlfile = "index.html";
 
-buf.write(fs.readFileSync(htmlfile, 'utf8');
+//buf.write(fs.readFileSync(htmlfile, 'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World' + buf.toString());
+  response.send('Hello World' + fs.readFileSync(htmlfile).toString());
 });
 
 var port = process.env.PORT || 5000;
