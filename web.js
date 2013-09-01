eaac7ac7e1d694ef03122aeefd25f0d001f1c682
wszdwp <pan.wang@stonybrook.edu>
2013-09-01 17:05:40 +0000

    update buffer read in web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = fs.readFileSync('index.html', 'utf8');
+
+var buf = new Buffer(256);
+
+buf.write(fs, 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World' + buf.toString());
 });
 
 var port = process.env.PORT || 5000;
